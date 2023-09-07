7cb0cb00c82b6b45e1f84fe20a1d303ba6e1a710
Floabs <florian.aberger@gmail.com>
2023-09-07 18:23:44 +0200

    changes

--- c_practice/helloworld.c
+++ c_practice/helloworld.c
@@ -1,7 +1,40 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<ctype.h>
 
-int main()
+struct Teppich {
+	int laenge;
+	int breite;
+	int anzahl_reihen_teppich_rand;
+	int anzahl_reihen_teppich_muster;
+	int dicke_teppich_muster;
+	int leerzeichen_teppich;
+};
+
+int main() {
+
+
+typedef struct Teppich Teppich;
+
+Teppich teppich;
+printf("Hallo, lass uns gemeinsam einen Teppich designen\n. Bitte gib die Länge deines Teppichs ein: \n");
+scanf("%d", &teppich.laenge);
+while (isdigit(teppich.laenge) == 0)
 {
-	printf("Hello World \n");
-	return 0;
+	printf("Die Eingabe war inkorrekt. \n Ich brauche bitte eine ganze Zahl fuer die Laenge des Teppichs. \n Versuche es erneut. Eine ganze Zahl bitte: ");
+	scanf("%d", &teppich.laenge);
 }
+printf("Wow ein %d langer Teppich, eine vorzügliche Wahl und weiter geht es mit dem Spass. \n Also sag mir bitte die Breite:  ", &teppich.laenge);
+scanf("%d", &teppich.breite);
+while (isdigit(teppich.breite) == 0)
+{
+	printf("Die Eingabe war inkorrekt. \n Ich brauche bitte eine ganze Zahl fuer die Breite des Teppichs ein. \n Versuche es erneut: %d", &teppich.laenge);
+	scanf("%d", &teppich.breite);
+}
+printf("Super! %d x %d, eine hervorragende Groesse  \n. Dann machen wir uns mal an das Muster  \n Wie dick haettest du denn deinen Rand gerne pro Seite?:  \n", &teppich.laenge, &teppich.breite);
+scanf("%d", &teppich.anzahl_reihen_teppich_rand);
+
+
+
+return 0;
+    }
--- teppich/teppich/teppich.c
+++ teppich/teppich/teppich.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<ctype.h>
 
 struct Teppich {
-	int teppich_laenge;
-	int teppich_breite;
+	int laenge;
+	int breite;
 	int anzahl_reihen_teppich_rand;
 	int anzahl_reihen_teppich_muster;
 	int dicke_teppich_muster;
@@ -11,6 +12,42 @@ struct Teppich {
 };
 
 typedef struct Teppich Teppich;
+
+Teppich teppich_groessen_ausleser()
+{
+	Teppich teppich;
+	printf("Hallo, lass uns gemeinsam einen Teppich designen\n. Bitte gib die Länge deines Teppichs ein: \n");
+	scanf("%d", &teppich.laenge);
+	while (isdigit(teppich.laenge) == 0)
+	{
+		printf("Die Eingabe war inkorrekt. \n Ich brauche bitte eine ganze Zahl fuer die Laenge des Teppichs. \n Versuche es erneut. Eine ganze Zahl bitte: ");
+		scanf("%d", &teppich.laenge);
+	}
+	printf("Wow ein %d langer Teppich, eine vorzügliche Wahl und weiter geht es mit dem Spass. \n Also sag mir bitte die Breite:  ", &teppich.laenge);
+	scanf("%d", &teppich.breite);
+	while (isdigit(teppich.breite) == 0)
+	{
+		printf("Die Eingabe war inkorrekt. \n Ich brauche bitte eine ganze Zahl fuer die Breite des Teppichs ein. \n Versuche es erneut: %d", &teppich.laenge);
+		scanf("%d", &teppich.breite);
+	}
+	printf("Super! %d x %d, eine hervorragende Groesse  \n. Dann machen wir uns mal an das Muster  \n Wie dick haettest du denn deinen Rand gerne pro Seite?:  \n", &teppich.laenge, &teppich.breite);
+	scanf("%d", &teppich.anzahl_reihen_teppich_rand);
+	while ((isdigit(teppich.anzahl_reihen_teppich_rand) == 0) || (teppich.laenge > (teppich.anzahl_reihen_teppich_rand * 2)))
+	{
+		printf("Die Eingabe war inkorrekt.\n Bitte beachte nur ganze Zahlen zu verwenden ausserdem kann der Rand nicht ueber die Gesamtlaenge des Teppichs hinaus gehen. \n Bitte um eine erneute Eingabe der gewuenschten breite pro Seite: \n");
+		scanf("%d", &teppich.anzahl_reihen_teppich_rand);
+	}
+	printf("Sehr schoen, ein %d breiter Rand oben und unten ist eine hervorragende Wahl. \n Und wie dick soll dein Muster pro Seite sein?: \n ", &teppich.anzahl_reihen_teppich_rand);
+	scanf("%d", &teppich.dicke_teppich_muster);
+	te
+
+
+	return teppich;
+}
+
+
+
+
 
 int main() {
 
